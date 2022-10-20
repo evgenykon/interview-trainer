f980e1b05fa9668a4acb43b579aa6e88c47cf5fc
effus <effusps@gmail.com>
2022-10-20 22:35:25 +0300

    github page deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue({jsx:true})]
+  integrations: [vue({jsx:true})],
+  site: 'https://evgenykon.github.io',
+  base: '/interview-trainer',
 });
